c54233b5f189f292ef60a54a9a05db150834c6c6
FatahillahAlif <11211039@student.itk.ac.id>
2023-10-27 12:34:49 +0800

    menyelesaikan 3 tabel

--- app/routes.php
+++ app/routes.php
@@ -9,96 +9,366 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
+
 return function (App $app) {
 
     // get
     $app->get('/barber', function (Request $request, Response $response) {
         $db = $this->get(PDO::class);
 
-        $query = $db->query('SELECT * FROM barber');
+        $query = $db->query('CALL SelectBarber()');
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $response->getBody()->write(json_encode($results));
 
         return $response->withHeader("Content-Type", "application/json");
     });
 
-    // get by id
-    $app->get('/countries/{id}', function (Request $request, Response $response, $args) {
+    $app->get('/costumer', function (Request $request, Response $response) {
         $db = $this->get(PDO::class);
 
-        $query = $db->prepare('SELECT * FROM countries WHERE id=?');
-        $query->execute([$args['id']]);
+        $query = $db->query('CALL SelectCostumer()');
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
-        $response->getBody()->write(json_encode($results[0]));
+        $response->getBody()->write(json_encode($results));
 
         return $response->withHeader("Content-Type", "application/json");
     });
 
-    // post data
-    $app->post('/countries', function (Request $request, Response $response) {
-        $parsedBody = $request->getParsedBody();
-
-        $id = $parsedBody["id"]; // menambah dengan kolom baru
-        $countryName = $parsedBody["name"];
-
+    $app->get('/detail_pembayaran', function (Request $request, Response $response) {
         $db = $this->get(PDO::class);
 
-        $query = $db->prepare('INSERT INTO countries (id, name) values (?, ?)');
+        $query = $db->query('CALL SelectPaymentDetail');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
 
-        // urutan harus sesuai dengan values
-        $query->execute([$id, $countryName]);
+        return $response->withHeader("Content-Type", "application/json");
+    });
 
+
+    $app->get('/service', function (Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL SelectService');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get('/service_menu', function (Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL SelectServiceMenu()');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+
+    // get by id
+    $app->get('/barber/{id}', function (Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+        $barber_id = $args['id'];
+    
+        try {
+            $query = $db->prepare('CALL SelectBarberByID(:barber_id)');
+            $query->bindParam(':barber_id', $barber_id, PDO::PARAM_INT);
+            $query->execute();
+    
+            $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    
+            if (empty($results)) {
+                $response->getBody()->write(json_encode(['error' => 'Data barber tidak ditemukan']));
+                return $response->withStatus(404)->withHeader("Content-Type", "application/json");
+            }
+    
+            $response->getBody()->write(json_encode($results[0]));
+            return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(['error' => 'Database error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        } catch (Exception $e) {
+            $response->getBody()->write(json_encode(['error' => 'Internal Server Error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        }
+    });
+
+
+    $app->get('/costumer/{id}', function (Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+        $costumer_id = $args['id'];
+    
+        try {
+            $query = $db->prepare('CALL SelectCostumerByID(:costumer_id)');
+            $query->bindParam(':costumer_id', $costumer_id, PDO::PARAM_INT);
+            $query->execute();
+    
+            $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    
+            if (empty($results)) {
+                $response->getBody()->write(json_encode(['error' => 'Data costumer tidak ditemukan']));
+                return $response->withStatus(404)->withHeader("Content-Type", "application/json");
+            }
+    
+            $response->getBody()->write(json_encode($results[0]));
+            return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(['error' => 'Database error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        } catch (Exception $e) {
+            $response->getBody()->write(json_encode(['error' => 'Internal Server Error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        }
+    });
+
+
+    $app->get('/detail_pembayaran/{id}', function (Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+        $paymentDetailId = $args['id'];
+    
+        try {
+            $query = $db->prepare('CALL SelectPaymentDetailByID(:payment_detail_id)');
+            $query->bindParam(':payment_detail_id', $paymentDetailId, PDO::PARAM_INT);
+            $query->execute();
+    
+            $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    
+            if (empty($results)) {
+                $response->getBody()->write(json_encode(['error' => 'Data rincian pembayaran tidak ditemukan']));
+                return $response->withStatus(404)->withHeader("Content-Type", "application/json");
+            }
+    
+            $response->getBody()->write(json_encode($results[0]));
+            return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(['error' => 'Database error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        } catch (Exception $e) {
+            $response->getBody()->write(json_encode(['error' => 'Internal Server Error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        }
+    });    
+    
+    
+    // post data
+    $app->post('/insertbarber', function (Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody();
+    
+        $barberName = $parsedBody["name"];
+    
+        $db = $this->get(PDO::class);
+    
+        $query = $db->prepare('CALL CreateBarberBaru(?)'); // Mengganti dengan pemanggilan procedure
+    
+        $query->execute([$barberName]);
+    
         $lastId = $db->lastInsertId();
-
+    
         $response->getBody()->write(json_encode(
             [
-                'message' => 'country disimpan dengan id ' . $lastId
+                'message' => 'Barber disimpan dengan ID ' . $lastId
             ]
         ));
-
+    
         return $response->withHeader("Content-Type", "application/json");
     });
+
+
+    $app->post('/insertcostumer', function (Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody();
+        $customerName = $parsedBody["name"];
+        $phoneNumber = $parsedBody["phone_number"];
+    
+        $db = $this->get(PDO::class);
+    
+        $query = $db->prepare('CALL CreateCostumerBaru(?, ?)'); // Mengganti dengan pemanggilan procedure
+    
+        $query->execute([$customerName, $phoneNumber]);
+    
+        $lastId = $db->lastInsertId();
+    
+        $response->getBody()->write(json_encode(
+            [
+                'message' => 'Costumer disimpan dengan ID ' . $lastId
+            ]
+        ));
+    
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+
+    $app->post('/insert_detail_pembayaran', function (Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody();
+    
+        $paymentMethod = $parsedBody["payment_method"];
+    
+        $db = $this->get(PDO::class);
+    
+        try {
+            $query = $db->prepare('CALL CreatePaymentDetail(:payment_method)');
+            $query->bindParam(':payment_method', $paymentMethod, PDO::PARAM_STR);
+            $query->execute();
+    
+            $lastId = $db->lastInsertId();
+    
+            $response->getBody()->write(json_encode(
+                [
+                    'message' => 'Detail pembayaran disimpan dengan ID ' . $lastId
+                ]
+            ));
+    
+            return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(['error' => 'Database error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        } catch (Exception $e) {
+            $response->getBody()->write(json_encode(['error' => 'Internal Server Error: ' . $e->getMessage()]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        }
+    });    
+
 
     // put data
-    $app->put('/countries/{id}', function (Request $request, Response $response, $args) {
+    $app->put('/updatebarber/{id}', function (Request $request, Response $response, $args) {
         $parsedBody = $request->getParsedBody();
-
         $currentId = $args['id'];
-        $countryName = $parsedBody["name"];
+        $barberName = $parsedBody["name"];
         $db = $this->get(PDO::class);
-
-        $query = $db->prepare('UPDATE countries SET name = ? WHERE id = ?');
-        $query->execute([$countryName, $currentId]);
-
-        $response->getBody()->write(json_encode(
-            [
-                'message' => 'country dengan id ' . $currentId . ' telah diupdate dengan nama ' . $countryName
-            ]
-        ));
-
+    
+        try {
+            $query = $db->prepare('CALL UpdateBarber(?, ?)');
+            $query->execute([$currentId, $barberName]);
+    
+            if ($query->rowCount() > 0) {
+                // Data telah diperbarui
+                $response->getBody()->write(json_encode(
+                    [
+                        'message' => 'Barber dengan ID ' . $currentId . ' telah diupdate dengan nama ' . $barberName
+                    ]
+                ));
+            } else {
+                // ID tidak ditemukan
+                $response->getBody()->write(json_encode(
+                    [
+                        'error' => 'Barber dengan ID ' . $currentId . ' tidak ditemukan'
+                    ]
+                ));
+                return $response->withStatus(404); // Status kode not found
+            }
+        } catch (Exception $e) {
+            // Penanganan kesalahan saat menjalankan permintaan ke database
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Terjadi kesalahan saat mengupdate Barber: ' . $e->getMessage()
+                ]
+            ));
+            return $response->withStatus(500); // Status kode kesalahan server
+        }
+    
         return $response->withHeader("Content-Type", "application/json");
     });
 
+
+    $app->put('/updatecostumer/{id}', function (Request $request, Response $response, $args) {
+        $parsedBody = $request->getParsedBody();
+        $currentId = $args['id'];
+        $newPhoneNumber = $parsedBody["phone_number"];
+        $db = $this->get(PDO::class);
+    
+        try {
+            $query = $db->prepare('CALL UpdateCostumer(?, ?)');
+            $query->execute([$currentId, $newPhoneNumber]);
+    
+            if ($query->rowCount() > 0) {
+                // Data telah diperbarui
+                $response->getBody()->write(json_encode(
+                    [
+                        'message' => 'Costumer dengan ID ' . $currentId . ' telah diupdate dengan nomor HP baru ' . $newPhoneNumber
+                    ]
+                ));
+            } else {
+                // ID tidak ditemukan
+                $response->getBody()->write(json_encode(
+                    [
+                        'error' => 'Costumer dengan ID ' . $currentId . ' tidak ditemukan'
+                    ]
+                ));
+                return $response->withStatus(404); // Status kode not found
+            }
+        } catch (Exception $e) {
+            // Penanganan kesalahan saat menjalankan permintaan ke database
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Terjadi kesalahan saat mengupdate Costumer: ' . $e->getMessage()
+                ]
+            ));
+            return $response->withStatus(500); // Status kode kesalahan server
+        }
+    
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+
+    $app->put('/update_payment_detail/{id}', function (Request $request, Response $response, $args) {
+        $parsedBody = $request->getParsedBody();
+        $currentId = $args['id'];
+        $newPaymentMethod = $parsedBody["name"];
+        $db = $this->get(PDO::class);
+    
+        try {
+            $query = $db->prepare('CALL UpdatePaymentDetailWithTransaction(?, ?)');
+            $query->execute([$currentId, $newPaymentMethod]);
+    
+            if ($query->rowCount() > 0) {
+                // Data telah diperbarui
+                $response->getBody()->write(json_encode(
+                    [
+                        'message' => 'Payment Detail dengan ID ' . $currentId . ' telah diupdate dengan metode pembayaran ' . $newPaymentMethod
+                    ]
+                ));
+            } else {
+                // ID tidak ditemukan
+                $response->getBody()->write(json_encode(
+                    [
+                        'error' => 'Payment Detail dengan ID ' . $currentId . ' tidak ditemukan'
+                    ]
+                ));
+                return $response->withStatus(404); // Status kode not found
+            }
+        } catch (Exception $e) {
+            // Penanganan kesalahan saat menjalankan permintaan ke database
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Terjadi kesalahan saat mengupdate Payment Detail: ' . $e->getMessage()
+                ]
+            ));
+            return $response->withStatus(500); // Status kode kesalahan server
+        }
+    
+        return $response->withHeader("Content-Type", "application/json");
+    });    
+
+
     // delete data
-    $app->delete('/countries/{id}', function (Request $request, Response $response, $args) {
+    $app->delete('/deletebarber/{id}', function (Request $request, Response $response, $args) {
         $currentId = $args['id'];
         $db = $this->get(PDO::class);
-
+    
         try {
-            $query = $db->prepare('DELETE FROM countries WHERE id = ?');
+            $query = $db->prepare('CALL DeleteBarber(?)'); // Mengganti dengan pemanggilan procedure
             $query->execute([$currentId]);
-
+    
             if ($query->rowCount() === 0) {
                 $response = $response->withStatus(404);
                 $response->getBody()->write(json_encode(
                     [
-                        'message' => 'Data tidak ditemukan'
+                        'error' => 'Data Barber dengan ID ' . $currentId . ' tidak ditemukan'
                     ]
                 ));
             } else {
                 $response->getBody()->write(json_encode(
                     [
-                        'message' => 'country dengan id ' . $currentId . ' dihapus dari database'
+                        'message' => 'Barber dengan ID ' . $currentId . ' dihapus dari database'
                     ]
                 ));
             }
@@ -106,11 +376,82 @@ return function (App $app) {
             $response = $response->withStatus(500);
             $response->getBody()->write(json_encode(
                 [
-                    'message' => 'Database error ' . $e->getMessage()
+                    'error' => 'Database error ' . $e->getMessage()
                 ]
             ));
         }
-
+    
         return $response->withHeader("Content-Type", "application/json");
     });
+
+
+    $app->delete('/deletecostumer/{id}', function (Request $request, Response $response, $args) {
+        $currentId = $args['id'];
+        $db = $this->get(PDO::class);
+    
+        try {
+            $query = $db->prepare('CALL DeleteCostumer(?)'); // Mengganti dengan pemanggilan procedure
+            $query->execute([$currentId]);
+    
+            if ($query->rowCount() === 0) {
+                $response = $response->withStatus(404);
+                $response->getBody()->write(json_encode(
+                    [
+                        'error' => 'Data Costumer dengan ID ' . $currentId . ' tidak ditemukan'
+                    ]
+                ));
+            } else {
+                $response->getBody()->write(json_encode(
+                    [
+                        'message' => 'Costumer dengan ID ' . $currentId . ' dihapus dari database'
+                    ]
+                ));
+            }
+        } catch (PDOException $e) {
+            $response = $response->withStatus(500);
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Database error ' . $e->getMessage()
+                ]
+            ));
+        }
+    
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+
+    $app->delete('/delete_detail_pembayaran/{id}', function (Request $request, Response $response, $args) {
+        $currentId = $args['id'];
+        $db = $this->get(PDO::class);
+    
+        try {
+            $query = $db->prepare('CALL DeleteDetailPembayaranWithTransaction(?)');
+            $query->execute([$currentId]);
+    
+            if ($query->rowCount() === 0) {
+                $response = $response->withStatus(404);
+                $response->getBody()->write(json_encode(
+                    [
+                        'error' => 'Data Detail Pembayaran dengan ID ' . $currentId . ' tidak ditemukan'
+                    ]
+                ));
+            } else {
+                $response->getBody()->write(json_encode(
+                    [
+                        'message' => 'Detail Pembayaran dengan ID ' . $currentId . ' dihapus dari database'
+                    ]
+                ));
+            }
+        } catch (PDOException $e) {
+            $response = $response->withStatus(500);
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Database error ' . $e->getMessage()
+                ]
+            ));
+        }
+    
+        return $response->withHeader("Content-Type", "application/json");
+    });
+    
 };
